57218e9997d8a46a8250d6464332b267a570ca2f
Stuart <stuart@yamartino.com>
2020-11-30 21:38:13 -0500

    First go!
    Added left nav and two example pages and getting things going

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
+    fontFamily: {
+      'serif': "'Inter', sans-serif",
+      'cursive': "'Meddon', cursive"
+    },
     extend: {},
   },
   variants: {},
